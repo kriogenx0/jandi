ea95b409d8a68ec2105c08bb8bf64d6c30bc0bb3
Alex Vaos <simplex0@gmail.com>
2012-10-25 21:41:33 -0700

    routes progress

--- jandi.js
+++ jandi.js
@@ -29,25 +29,44 @@
 	$.app.debug = function() {
 		// arguments;
 	};
-    $.app.route = function( r, url ) {
-        if (!r) r = this.routes;
-        if (!url) url = location.pathname.toLowerCase();
-		// $.debug( ['routes', r, url]);
-		for( i in r ) {
-			//if (r[i].pattern ) {
-				//$.debug( ['r', r[i].pattern, r[i].view, url.match( r[i].pattern ) ] );
-			if (r[i].pattern && r[i].view &&
-				( url.match( r[i].pattern ) || ( r[i].pattern.test && r[i].pattern.test( url ) ) )
-			) {
-				$.debug( ['LOADING ROUTE', r[i] ] );
-				if (r[i].view.init) {
-					$(r[i].view.init);
-				} else if ($.type(r[i].view) == 'function')
-					$(r[i].view);
-				break;
-			}
-		}
-	};
+  $.app.route = function( r, url ) {
+    if (!r) r = this.routes;
+    if (!url) url = location.pathname.toLowerCase().replace(/^\/+/g,'');
+    // $.debug( ['routes', r, url]);
+
+    for( i in r ) {
+      //$.debug( ['r', r[i].pattern, r[i].view, url.match( r[i].pattern ) ] );
+      $.debug( ['LOADING ROUTE', r[i] ] );
+
+      // CHECK IF ROUTE IS STRING
+      if (typeof r[i] == 'string') {
+        if (typeof app.view[r[i]] == 'function')
+          $(app.view[r[i]]);
+        else if (app.view[r[i]].init)
+          $(app.view[r[i]].init);
+      }
+      else if
+        (
+          r[i].pattern && r[i].view &&
+          ( url.match( r[i].pattern ) || ( r[i].pattern.test && r[i].pattern.test( url ) ) )
+        )
+      {
+        /*
+        if (r[i].view.init) {
+          $(r[i].view.init);
+        } else if ($.type(r[i].view) == 'function') {
+          $(r[i].view);
+        }
+       */
+        if (typeof r[i].view == 'function')
+          $(r[i].view);
+        else if (app.view[r[i]].init)
+          $(app.view[r[i]].init);
+        break;
+      }
+    }
+  };
+
 	$.app.init = function() {
 		$.app.detectEnvironment();
 		if ($.app.routes)
